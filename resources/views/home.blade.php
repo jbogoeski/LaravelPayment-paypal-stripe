f1fe0df4e0580507c2d351c7227b33dfa742d540
j_bogoeski@hotmail.com <j_bogoeski@hotmail.com>
2020-11-10 22:10:46 +0100

    Created login ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Welcome!</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    This is payments processing system with PayPal and Stripe.                    
+                 
+
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
