9f3bed26d327c21b94f661d14ddf8b0c49722f62
j_bogoeski@hotmail.com <j_bogoeski@hotmail.com>
2020-11-10 21:41:38 +0100

    Database and Models created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\CurrenciesTableSeeder;
+use Database\Seeders\PaymentPlatformsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call(PaymentPlatformsTableSeeder::class);
+        $this->call(CurrenciesTableSeeder::class);
     }
 }
